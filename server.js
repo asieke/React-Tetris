428bb6b6b7f6a2ec872612a7c8fd32942edb77d6
Alex Sieke <asieke@gmail.com>
2021-12-18 13:38:04 -0500

    configuring sequelize to point to heroku psql

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
-  dialect: 'postgres',
-  host: process.env.PG_HOST
-})
+
+const sequelize = (process.env.NODE_ENV === 'development')
+  ? new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
+    dialect: 'postgres',
+    host: process.env.PG_HOST
+  })
+  : new Sequelize(process.env.DATABASE_URL)
+
+
 const express = require("express");
 const app = express();
 app.use(express.json());
