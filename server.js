23e63fdf08e5aed7052680ab066925629609dd6c
Alex Sieke <asieke@gmail.com>
2021-12-18 13:13:26 -0500

    refactored to psql

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
+require('dotenv').config()
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('tetris', 'root', '', {
-  dialect: 'mysql',
-  host: 'localhost'
+const sequelize = new Sequelize('tetris', process.env.DB_USER, process.env.DB_PASS, {
+  dialect: 'postgres',
+  host: process.env.DB_HOST
 })
 const express = require("express");
 const app = express();
